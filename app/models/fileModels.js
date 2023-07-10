93bbbd22f10a9b5215c0d6f22b3b411a06e6d903
farizi567 <malfarizisidiqrizki015@gmail.com>
2023-07-11 03:00:03 +0700

    menambahkan timestamp

--- app/models/fileModels.js
+++ app/models/fileModels.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
-const formSchema = new mongoose.Schema({
-  nama: String,
-  nim: String,
-  prodi: String,
-  file: String,
-});
+const formSchema = new mongoose.Schema(
+  {
+    nama: String,
+    nim: String,
+    prodi: String,
+    file: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Form = mongoose.model("Form", formSchema);
 
