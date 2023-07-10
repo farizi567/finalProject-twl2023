665f87e7b006f1d7c001cc6f993ab99762994320
farizi567 <malfarizisidiqrizki015@gmail.com>
2023-07-11 03:01:38 +0700

    menambahkan require untuk upload ke google drive

--- app/controllers/fileController.js
+++ app/controllers/fileController.js
@@ -1,4 +1,5 @@
 const Form = require("../models/fileModels");
+const { uploadToGoogleDrive } = require("../service/googleDriveService");
 
 const uploadFile = async (req, res) => {
   try {
@@ -9,16 +10,22 @@ const uploadFile = async (req, res) => {
       return res.status(400).json({ message: "Missing required fields" });
     }
 
-    const newForm = new Form({
-      nama: nama,
-      nim: nim,
-      prodi: prodi,
-      file: file.filename,
-    });
+    const response = await uploadToGoogleDrive(file);
 
-    await newForm.save();
+    if (response.status === 200) {
+      const newForm = new Form({
+        nama: nama,
+        nim: nim,
+        prodi: prodi,
+        file: file.filename,
+      });
 
-    return res.status(200).json({ message: "Form data uploaded successfully" });
+      await newForm.save();
+    }
+
+    return res
+      .status(200)
+      .json({ data: response, message: "Form data uploaded successfully" });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
