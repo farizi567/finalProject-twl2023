136a95dd93a366009f0796a743003add1decb137
farizi567 <malfarizisidiqrizki015@gmail.com>
2023-07-11 22:42:31 +0700

    mengubah avatar user

--- pages/index.vue
+++ pages/index.vue
@@ -3,47 +3,15 @@
     <div
       class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
     >
-      <div class="flex justify-end px-4 pt-4">
-        <!-- Dropdown menu -->
-        <div
-          id="dropdown"
-          class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-        >
-          <ul class="py-2" aria-labelledby="dropdownButton">
-            <li>
-              <a
-                href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-              >Edit</a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-              >Export Data</a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-              >Delete</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="flex flex-col items-center pb-10">
-        <h1 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-          Welcome
+      <div class="flex flex-col items-center pb-10 gap-5">
+        <h1 class="mb-1 text-xl font-medium text-gray-900 dark:text-white mt-5">
+          Selamat Datang
         </h1>
         <img
-          class="w-24 h-24 mb-3 rounded-full shadow-lg"
-          src="../static/user.jpeg"
+          class="w-24 h-24 rounded-full shadow-lg"
+          src="../static/profile-user.png"
           alt="Bonnie image"
         >
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-          Bonnie Green
-        </h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
         <div class="flex mt-4 space-x-3 md:mt-6">
           <NuxtLink
             to="/login"
