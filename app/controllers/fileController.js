5e71a6fd1f90efa260134f65aad020639602b529
farizi567 <malfarizisidiqrizki015@gmail.com>
2023-07-18 00:59:08 +0700

    menambahkan function untuk mengambil data dan menghapus data file

--- app/controllers/fileController.js
+++ app/controllers/fileController.js
@@ -1,5 +1,5 @@
 const Form = require("../models/fileModels");
-const { uploadToGoogleDrive } = require("../service/googleDriveService");
+const { uploadToGoogleDrive, deleteFileFromGoogleDrive, getAllFilesFromGoogleDrive   } = require("../service/googleDriveService");
 
 const uploadFile = async (req, res) => {
   try {
@@ -18,6 +18,7 @@ const uploadFile = async (req, res) => {
         nim: nim,
         prodi: prodi,
         file: file.filename,
+        googleDriveId: response.data.id, // Simpan ID Google Drive dalam dokumen MongoDB
       });
 
       await newForm.save();
@@ -26,12 +27,52 @@ const uploadFile = async (req, res) => {
     return res
       .status(200)
       .json({ data: response, message: "Form data uploaded successfully" });
+      
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
   }
 };
 
+const getDataFromGoogleDrive = async (req, res) => {
+  try {
+    // 1. Ambil data file dari Google Drive menggunakan Google Service
+    const files = await getAllFilesFromGoogleDrive();
+
+    // 2. Ambil data dari MongoDB if needed
+    const data = await Form.find();
+
+    // Mengembalikan respons dengan data file dan data dari MongoDB
+    res.status(200).json({ files, data });
+  } catch (error) {
+    console.error('Error getting data:', error);
+    res.status(500).json({ message: 'Failed to get data' });
+  }
+};
+
+const deleteFile = async (req, res) => {
+  const fileId = req.params.id; // Mendapatkan ID file dari parameter URL atau request body
+
+  try {
+    const file = await Form.findOneAndDelete({ googleDriveId: fileId }); // Menghapus dokumen berdasarkan googleDriveId
+
+    if (!file) {
+      return res.status(404).json({ message: 'File not found' });
+    }
+ 
+    await deleteFileFromGoogleDrive(fileId);
+    
+    res.status(200).json({ message: 'File deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting file:', error);
+    res.status(500).json({ message: 'Failed to delete file' });
+  }
+};
+
+
+
 module.exports = {
   uploadFile,
+  getDataFromGoogleDrive,
+  deleteFile,
 };
