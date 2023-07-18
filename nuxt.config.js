ce395f9c50b2e6141e9d7f7f11ef91d5f561386e
farizi567 <malfarizisidiqrizki015@gmail.com>
2023-07-18 17:59:05 +0700

    memperbaharui baseUrl menggunakan final project

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://twl2023-2100016127-p6-7.vercel.app/'
+    baseURL: 'https://final-project-twl2023.vercel.app/'
   },
 
   auth: {
