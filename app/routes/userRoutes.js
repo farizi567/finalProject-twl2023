fd7757c047ee9cacde947dfca0dd2453b5910050
farizi567 <malfarizisidiqrizki015@gmail.com>
2023-07-18 15:42:57 +0700

    menambahkan router update file

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { registerUser, loginUser, getDataUser } = require("../controllers/userController");
 const { fileHandler } = require("../../middleware/multerHandler");
 const fileController = require("../controllers/fileController");
-const {getDataFromGoogleDrive, deleteFile} = require("../controllers/fileController");
+const {getDataFromGoogleDrive, deleteFile, updateFileName} = require("../controllers/fileController");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
@@ -13,5 +13,6 @@ router.route("/me").get(getDataUser);
 router.route("/upload").post(fileHandler.single("file"), fileController.uploadFile);
 router.route("/getFiles" ).get(getDataFromGoogleDrive)
 router.route("/files/:id").delete(deleteFile);
+router.route("/files/:id").put(updateFileName)
 
 module.exports = router;
